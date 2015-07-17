b3a3fcb7bc2cf28cfe69b7d3a14268ad8d4791fa
endows <you@example.com>
2015-07-18 01:54:22 +0900

    insert Site

--- User.js
+++ User.js
@@ -3,7 +3,6 @@ Users = Meteor.users
 if(Meteor.isServer){
   Meteor.publish('users',function(friends){
     var current_user = Users.findOne(this.userId)
-    console.log(current_user.profile.friends)
     return Users.find({_id:{$in:current_user.profile.friends.map(String)}})
   })
 }

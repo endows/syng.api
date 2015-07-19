2398a2db93d8ea9a0176a16a4ca03cefacaa32ba
endows <you@example.com>
2015-07-19 15:14:52 +0900

    login page

--- pages/home.js
+++ pages/home.js
@@ -1,13 +1,18 @@
 Router.route('/home',{
-  waitOn:function(){
-    return [Meteor.subscribe('allStatus'),Meteor.subscribe('users')]
-  },
+  // waitOn:function(){
+  //   return [Meteor.subscribe('allStatus'),Meteor.subscribe('users')]
+  // },
   data:{
     status:function(){
       return Status.find()
     }
   },
   action:function(){
+    Meteor.subscribe('allStatus')
+    Meteor.subscribe('users')
+    // if(!Meteor.userId()){
+    //   Meteor.loginWithTwitter()
+    // }
     this.render('home')
   }
 })
